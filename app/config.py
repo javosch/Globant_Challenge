dac233a46d2000c5288a1c403317ee7b0474c83b
JavierSchmitt <javo.sch@gmail.com>
2024-12-15 23:29:18 -0300

    Se agrega funcionalidad para subir archivos csv

--- app/config.py
+++ app/config.py
@@ -1,13 +1,16 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData, insert, delete
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import URL
+
+DATABASE = 'globant_challenge'
+
 
 DATABASE_URL = URL.create(
     drivername='postgresql'
     , username='globant'
     , password='globant'
     , host='localhost'
-    , database='globant_challenge'
+    , database=DATABASE
     , port=5432
 )
 
@@ -18,3 +21,28 @@ Session = sessionmaker(bind=engine)
 
 def init_db():
     metadata.create_all(engine)
+
+    data_catalog = [
+        [1, 'departments', 'departments__1___1_'],
+        [2, 'jobs', 'jobs'],
+        [3, 'hired_employees', 'hired_employees__1___1_']
+    ]
+
+    table = metadata.tables['catalog_tables']
+
+    for data in data_catalog:
+        stmt_delete = delete(table).where(
+            table.columns.id.in_([data[0]])
+        )
+        
+        stmt_insert = insert(table).values(
+            id=data[0],
+            table_name=data[1],
+            file_name=data[2]
+        )
+
+        
+        with engine.connect() as conn:
+            conn.execute(stmt_delete)
+            conn.execute(stmt_insert)
+            conn.commit()
--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from sqlalchemy import Table, Column, Integer, String, DateTime
 from app.config import metadata
 
+catalog_tables = Table(
+    'catalog_tables', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('table_name', String, nullable=False),
+    Column('file_name', String, nullable=False)
+)
+
 departments = Table(
     'departments', metadata,
     Column('id', Integer, primary_key=True),
@@ -16,8 +23,8 @@ jobs = Table(
 hired_employees = Table(
     'hired_employees', metadata,
     Column('id', Integer, primary_key=True),
-    Column('name', String, nullable=False),
-    Column('datetime', DateTime, nullable=False),
-    Column('department_id', Integer, nullable=False),
-    Column('job_id', Integer, nullable=False)
+    Column('name', String, nullable=True),
+    Column('datetime', DateTime, nullable=True),
+    Column('department_id', Integer, nullable=True),
+    Column('job_id', Integer, nullable=True)
 )
