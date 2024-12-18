ec665fff9dc9d06d71dd9257d8cb14a0e25f46f8
JavierSchmitt <javo.sch@gmail.com>
2024-12-18 01:46:13 -0300

    Se agregan funciones de utilidades y se ordena más el código.
    Funciones nuevas:
    - df_to_schema
    - process_csv (se cambia de archivo)
    - insert_records
    - update_records
    - delete_records

--- app/operations.py
+++ app/operations.py
@@ -1,59 +1,9 @@
 from fastapi import FastAPI, UploadFile, BackgroundTasks, HTTPException
-from app.models import metadata
-from app.config import engine
-from sqlalchemy import select, delete
-import numpy as np
-import pandas as pd
 import os
 
-
-# Process CSV and upload to DB
-def process_csv(file_path: str):
-    try:
-        # Infer table name from file name
-        table_name = os.path.splitext(os.path.basename(file_path))[0]
-
-        catalog_table_sql = 'SELECT table_name, file_name FROM catalog_tables'
-        catalog_table_data = pd.read_sql(catalog_table_sql, con=engine).set_index('file_name')
-        map_names_tables = catalog_table_data.to_dict()['table_name']
-
-        try:
-            table_name = map_names_tables[table_name]
-            table = metadata.tables.get(table_name)
-        except KeyError:
-            table = metadata.tables.get(table_name)
-
-        if table is None:
-            raise ValueError(f"Table {table_name} does not exist in the database.")
-
-        # Get columns names
-        cols = [col.name for col in table.columns.values()]
-
-        # Load CSV data
-        df = pd.read_csv(
-            file_path
-            , sep=','
-            , names=cols
-        )
-
-        # Change column datetime to datetime format
-        if table_name == "hired_employees":
-            df["datetime"] = pd.to_datetime(df["datetime"]).replace({np.nan: None})
-
-        # Replace null values with None, the engine works with None
-        df = df.replace({np.nan: None})
-
-        with engine.begin() as conn:
-            conn.execute(table.insert(), df.to_dict(orient="records"))
-        
-        return True
-
-    except Exception as e:
-        error = f"Error processing file {file_path}: {str(e)}"
-        print(error)
-        return error
-    finally:
-        os.remove(file_path)
+from app.models import metadata
+from app.config import engine
+from app.utils import process_csv, insert_records
 
 
 def init_operations(app: FastAPI):
@@ -81,10 +31,12 @@ def init_operations(app: FastAPI):
             # background_tasks.add_task(process_csv, file_path)
 
             result = process_csv(file_path=file_path)
-            print(result)
 
-            if result == True:
-                return {"message": f"File {file.filename} uploaded and processing started."}
+            if result.status_code == 200:
+                # TODO  Debo poner un return acá, la API devolverá como mensaje el dict que ponga
+                # TODO  Dejarlo igual que el resto de funciones de utils.py
+                print({"message": f"File {file.filename} uploaded and processing started."})
+
             else:
                 raise Exception(result)
 
